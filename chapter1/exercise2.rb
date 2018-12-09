6ca290309e92e82ccc41c884b281a63665821adb
katorihuela <43284037+katorihuela@users.noreply.github.com>
2018-12-09 18:21:10 -0500

    Update exercise2.rb

--- chapter1/exercise2.rb
+++ chapter1/exercise2.rb
@@ -3,6 +3,11 @@
 x = 9876
 
 thousands = x / 1000
-hundreds = x / 1000 / 100
-tens = x / 1000 / 100 / 10
-ones = x / 1000 / 100 / 10 / 10
+hundreds = x % 1000 / 100
+tens = x % 1000 % 100 / 10
+ones = x % 1000 % 100 % 10
+
+puts thousands
+puts hundreds
+puts tens
+puts ones
