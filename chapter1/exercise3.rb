786ebaa0723887a9b07c6d8656cccdafddc04068
katorihuela <43284037+katorihuela@users.noreply.github.com>
2018-12-09 18:22:39 -0500

    Create exercise3.rb

---
+++ chapter1/exercise3.rb
@@ -0,0 +1,12 @@
+# So I did this "Codecademy style" -- I'm not even sure if the syntax is correct, but it runs properly in the command line, so I guess it's OK. LOL
+
+movies = {
+  "Amelie" => 2001,
+  "Ever After" => 1998,
+  "While You Were Sleeping" => 1992,
+  "Tombstone" => 1990
+}
+
+love = movies.each { |x, y| puts y }
+
+puts love
