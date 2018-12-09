3d5075ac08067ef02f7f374ad1ffb04b285625c9
katorihuela <43284037+katorihuela@users.noreply.github.com>
2018-12-09 01:12:25 -0500

    Create exercise2.rb

---
+++ chapter1/exercise2.rb
@@ -0,0 +1,8 @@
+# This was a bit confusing because I wasn't sure what you guys were asking for exactly, so I looked at the solution fairly quicioly after fumbling for a couple of minutes
+
+x = 9876
+
+thousands = x / 1000
+hundreds = x / 1000 / 100
+tens = x / 1000 / 100 / 10
+ones = x / 1000 / 100 / 10 / 10
